830c8cfd75db07cf0af890cf7c961e70f51b1257
Natty Teshome <nattyteshome30@gmail.com>
2022-06-23 16:16:45 -0500

    initial commit

---
+++ src/main/java/com/example/mppproject/MppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mppproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mppproject/MppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mppproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
