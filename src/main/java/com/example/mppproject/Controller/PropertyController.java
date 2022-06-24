6979606017e3abc83017536e1c6cbf18a319b956
Michael Demewoz <gdmichaeldemewoz@gmail.com>
2022-06-24 17:32:54 -0500

    create function signature created

--- src/main/java/com/example/mppproject/Controller/PropertyController.java
+++ src/main/java/com/example/mppproject/Controller/PropertyController.java
@@ -1,9 +1,23 @@
 package com.example.mppproject.Controller;
 
+import com.example.mppproject.Service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1/property")
 public class PropertyController {
+
+    private final PropertyService propertyService;
+
+    @Autowired
+    public PropertyController(PropertyService propertyService){
+        this.propertyService = propertyService;
+    }
+
+    @PostMapping
+    public void createProperties(){}
+
 }
