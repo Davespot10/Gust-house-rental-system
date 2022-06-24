12c9eadd56171b952452ed2f07f0044294f79da3
Michael Demewoz <gdmichaeldemewoz@gmail.com>
2022-06-24 17:25:14 -0500

    add starter url to property controller

--- src/main/java/com/example/mppproject/Controller/PropertyController.java
+++ src/main/java/com/example/mppproject/Controller/PropertyController.java
@@ -1,7 +1,9 @@
 package com.example.mppproject.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "api/v1/property")
 public class PropertyController {
 }
