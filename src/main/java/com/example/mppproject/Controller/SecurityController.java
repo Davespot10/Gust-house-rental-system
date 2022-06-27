90009aba41ff3840830dd4c61f1f58ff96947f93
Natty Teshome <nattyteshome30@gmail.com>
2022-06-26 22:37:19 -0500

    add security controller

---
+++ src/main/java/com/example/mppproject/Controller/SecurityController.java
@@ -0,0 +1,48 @@
+package com.example.mppproject.Controller;
+
+import com.example.mppproject.Model.authentication.AuthenticationRequest;
+import com.example.mppproject.Model.authentication.AuthenticationResponse;
+import com.example.mppproject.Service.MyUserDetailService;
+import com.example.mppproject.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private MyUserDetailService myUserDetailService;
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<AuthenticationResponse> createAuthenticationToken
+            (@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken
+                            (authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        }
+        catch (BadCredentialsException e){
+            throw new BadCredentialsException("Incorrect username or password ", e);
+        }
+        final UserDetails userDetails = myUserDetailService
+                .loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+
+}
+
