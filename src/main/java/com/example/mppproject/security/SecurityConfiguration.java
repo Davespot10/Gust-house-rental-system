6838eb2df3a5fa018757767926daeb1b8ed0be0b
Natty Teshome <nattyteshome30@gmail.com>
2022-06-30 16:45:32 -0500

    add cros

--- src/main/java/com/example/mppproject/security/SecurityConfiguration.java
+++ src/main/java/com/example/mppproject/security/SecurityConfiguration.java
@@ -28,8 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().
-                disable().
+        http.cors().and().
                 authorizeRequests()
                 .antMatchers("/api/v1/login").permitAll()
                 .antMatchers("/api/v1/property").permitAll()
