a5ff64aa84b5849cf5f3702eda71a9b4287ee1df
dawitdemelashgedefaw <davespot100@gmail.com>
2022-07-02 11:24:45 -0500

    2 test passed

--- src/test/java/com/example/mppproject/Repository/AppUserRepositoryTest.java
+++ src/test/java/com/example/mppproject/Repository/AppUserRepositoryTest.java
@@ -18,6 +18,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -38,6 +40,17 @@ class AppUserRepositoryTest {
         AppUser appUser=new AppUser(RandomGenerator.generateName(5),RandomGenerator.generateName(5),RandomGenerator.generateEmail(),role,"123456",address,account);
         Mockito.when(appUserRepository.save(appUser)).thenReturn(appUser);
         assertEquals(appUser,appUserService.addAppUser(appUser));
+    }
+    @Test
+    public void getAppUser(){
+        Account account=new Account(RandomGenerator.generateAccount(),672.2);
+        Address address=new Address("Addis Ababa","Addis Ababa","Ethiopia","1234","5678","19.2","38.9");
+        Set<Role> role=new HashSet<>();
+        role.add(new Role(RoleType.GUEST));
+        AppUser appUser=new AppUser("Dawit","Demelash", RandomGenerator.generateEmail(),role,"123456",address,account);
+        Mockito.when(appUserRepository.findAll()).thenReturn(Stream.of(appUser).collect(Collectors.toList()));
+        assertEquals(1,appUserService.getAppUser().size());
+
     }
     @Test
     void otherContextLoads() {
